52c932a00b7e741fc9509cb1961e2c2cc61c7414
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-18 17:51:46 +0900

    understanding difference of usage of ++

---
+++ plusplus.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(Strig args[]){
+        int  num = 25;
+        int result = num++;
+        System.out.println("num : " + num);
+        System.out.println("result : " + result);
+        
+        num = 5;
+        
+        int result2 = ++num;
+        System.out.println("num : " + num);
+        System.out.println("result2 : " + result2);
+    }
+}    
