230b80ee5e1bb1890cdbaad18926ed20cb420db9
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-21 22:04:20 +0900

    Create Multiplication99.java
    multiplication table 
    print 2 to 81

---
+++ Multiplication99.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+        for (int i = 1; i < 9; i++){
+            for (int j = 0; j < 9; j++){
+                System.out.println((i+1)*(j+1));
+            }
+        }
+    }
+}  
