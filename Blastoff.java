a4e8f116d8a5dcb9eb9a55a9c0388e9c413a214f
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-21 22:24:43 +0900

    Create Blastoff.java
    Blastoff!
    10 9 8 7 6 5 4 3 2 1 launch!

---
+++ Blastoff.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String args[]) {
+        int count = 10;
+        while (count >= 0) {
+            if (count == 0){
+                System.out.println("로켓 발사!");
+            }
+            else{
+                System.out.println(count);
+            }
+            count--;
+        }
+    }
+}    
+                
