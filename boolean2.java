29d8b2df790b6b0c32e81a08c4165dc9cf185a13
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-21 16:59:26 +0900

    Create boolean2.java
    print
    true, false, true

---
+++ boolean2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String args[]) {
+        boolean d7 = true || false;
+        boolean d8 = true && false;
+        boolean d9 = (8 < 9) || (11 < 10);
+      
+        System.out.println(d7 +", "+ d8 +", "+ d9);
+    }
+}  
