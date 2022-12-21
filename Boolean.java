aa096b4ab7f9cbbfe736deccd9650846efbc2b76
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-21 16:43:36 +0900

    Create Boolean.java
    print
    true, false, true

---
+++ Boolean.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String args[]) {
+        boolean c4 = 5 < 7;
+        boolean c5 = 6 != 6;
+        boolean c6 = 7 > 5;
+        System.out.println(c4 + ", " + c5 + ", " + c6);
+    }
+}  
