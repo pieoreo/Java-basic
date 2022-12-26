62e877f9468fbfaed0a3e807b1bf6bf41fd650b8
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-26 23:02:54 +0900

    Create IarrayLength.java

---
+++ IarrayLength.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        int [] iArray = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
+        System.out.println("length of iArray : " + iArray.length);
+        System.out.println("last value of iArray : " + iArray[iArray.length-1]);
+    }
+}  
