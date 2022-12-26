7aa4b8629be58958d6327b59aac0dfdd45ea0d78
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-26 22:55:29 +0900

    Create Iarray.java

---
+++ Iarray.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String args[]) {
+        int [] iArray = new int[3];
+        iArray[0] = 1;
+        iArray[1] = 2;
+        iArray[2] = 3;
+        
+        System.out.println("iArray[0] : " + iArray[0]);
+        System.out.println("iArray[1] : " + iArray[1]);
+        System.out.println("iArray[2] : " + iArray[2]);
+    }
+}  
