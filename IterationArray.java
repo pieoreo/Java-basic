b74ae7eba8b6f480a92ce3162bcc4bbf056ac79d
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-12-26 23:40:18 +0900

    Create IterationArray.java

---
+++ IterationArray.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        int[] iArray = new int[] {1, 2, 3, 4, 5};
+        for (int i = 0; i<iArray.length; i++) {
+            System.out.println(iArray[i]);
+        }
+    }
+}  
