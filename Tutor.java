6c1ad9d481012d03c876695a7c2fdb06403c64da
pieoreo <72796740+pieoreo@users.noreply.github.com>
2023-01-04 20:55:02 +0900

    Create Tutor.java

---
+++ Tutor.java
@@ -0,0 +1,26 @@
+public class Tutor {
+    private String name;
+    private String bio;
+    
+    //basic constructor
+    public Tutor(){
+    }
+    //constructor
+    public Tutor(String name, String bio){
+        this.name = name;
+        this.bio = bio;
+    //setter
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setBio(String bio){
+        this.bio = bio;
+    }
+    //getter
+    public String getName(){
+        return this.name = name;
+    }
+    public String getBio(){
+        return this.bio = bio;
+    }  
