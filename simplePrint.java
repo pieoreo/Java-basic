a108e283a1096bbd7e879e59c5456976c9366c97
pieoreo <72796740+pieoreo@users.noreply.github.com>
2023-01-04 23:47:32 +0900

    Create simplePrint.java

---
+++ simplePrint.java
@@ -0,0 +1,16 @@
+//parameter yes, return yes
+public void simplePrint() {
+    System.out.println("no parameter, no return!");
+}
+//parameter yes, return no
+public void simpleSum(int num1, int num2) {
+    System.out.println("num1 :" + num1 + ", num2: " + num2);
+}
+//parameter no, return o
+public int simpleReturn() {
+    return 3;
+}
+//parameter o , return o
+public int sum(int num1, int num2) {
+    return num1 + num2;
+}  
